a872a2fe98155e94ffa1cec5a43b09e027770114
Jérôme Lebel <jerome@fotonauts.com>
2014-08-02 23:33:22 +0200

    new version

--- merge-from-spark.sh
+++ merge-from-spark.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-cd core-common-lib
-git pull
-cd ..
+pushd . > /dev/null
+tools_path="`pwd`/$0"
+cd `dirname $tools_path`
 
-cd core-communication-lib
-git pull
-cd ..
+merge_project() {
+    project="$1"
+    cd "${project}"
+    git checkout spark-core-master
+    if [ "$?" = "1" ] ; then
+        git remote add spark-core "git@github.com:spark/${project}.git"
+        git fetch spark-core
+        git branch spark-core-master spark-core/master
+        git checkout spark-core-master
+    fi
+    git pull
+    git checkout master
+    git merge spark-core-master
+    cd ..
+}
 
 cd core-firmware
 rm -fr inc src
@@ -19,5 +31,8 @@ git merge spark-master
 git rm build/core-firmware.bin
 git rm build/core-firmware.elf
 git rm build/core-firmware.hex
-git commit
-git push
+cd ..
+
+merge_project core-common-lib
+merge_project core-communication-lib
+merge_project core-firmware
