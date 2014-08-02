af39241ce1784a20e3d3e6bfa3f50ab4bed076f9
Jérôme Lebel <jerome@fotonauts.com>
2014-08-02 23:25:12 +0200

    new version according to the new settings

--- update-project.sh
+++ update-project.sh
@@ -3,19 +3,8 @@
 pushd . > /dev/null
 tools_path="`pwd`/$0"
 cd `dirname $tools_path`
-cd ../..
 
-cd core-firmware
 git checkout master
-git pull
-cd ../core-common-lib
-git checkout master
-git pull
-cd ../core-communication-lib
-git checkout master
-git pull
-cd ..
-
-git commit -m "latest core" core-firmware core-common-lib core-communication-lib
+git submodule update --init --recursive
 
 popd > /dev/null
